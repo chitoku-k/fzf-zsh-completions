7598b9e67b9af53681e1c72fd15ac0a1eecfbff2
Hexin <hexium310@gmail.com>
2020-01-04 00:16:55 +0900

    Fix according to reviews.

--- git.zsh
+++ git.zsh
@@ -70,31 +70,33 @@ _fzf_complete_git-add() {
     _fzf_complete '--ansi' "$@" < <(git status --porcelain=v1 2> /dev/null | awk \
         -v green="$(tput setaf 2)" \
         -v red="$(tput setaf 1)" \
-        -v reset="$(tput sgr0)" "
-        $fzf_colorize_git_status
-        /^.[^ ]/ {
-            y = substr(\$0, 2, 1)
-            print colorize_git_status(green, red, reset)
-        }
-        ")
+        -v reset="$(tput sgr0)" '
+            '"$_fzf_complete_awk_functions"'
+            /^.[^ ]/ {
+                print colorize_git_status(green, red, reset)
+            }
+        '
+    )
 }
 
 _fzf_complete_git-add_post() {
     awk '{ print substr($0, 4) }'
 }
 
-fzf_colorize_git_status='function colorize_git_status(color1, color2, reset) {
-    x = substr($0, 1, 1)
-    y = substr($0, 2, 1)
-    if (x ~ /[MADRC]/) {
-        xcolor = color1
-    }
-    if (x y ~ /(D[DU]|A[AU])|U.|\?\?|!!/) {
-        xcolor = color2
-    }
-    if (y ~ /[MADRCU\?!]/) {
-        ycolor = color2
-    }
+_fzf_complete_awk_functions='
+    function colorize_git_status(color1, color2, reset) {
+        index_status = substr($0, 1, 1)
+        work_tree_status = substr($0, 2, 1)
+        if (index_status ~ /[MADRC]/) {
+            index_status_color = color1
+        }
+        if (index_status work_tree_status ~ /(D[DU]|A[AU])|U.|\?\?|!!/) {
+            index_status_color = color2
+        }
+        if (work_tree_status ~ /[MADRCU\?!]/) {
+            work_tree_status_color = color2
+        }
 
-    return sprintf("%s%s%s%s%s%s %s", xcolor, x, reset, ycolor, y, reset, substr($0, 4))
-}'
+        return sprintf("%s%s%s%s%s%s %s", index_status_color, index_status, reset, work_tree_status_color, work_tree_status, reset, substr($0, 4))
+    }
+'
