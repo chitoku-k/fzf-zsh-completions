38adb4828472bee8e0e13683fa013e4aceb34085
Hexin <hexium310@gmail.com>
2020-01-13 19:37:25 +0900

    Add completion for all options that take values of `git commit`
    - --reuse-message/-C
    - --reedit-message/-c
    - --fixup
    - --squash
    - --message/-m
    - --author
    - --date
    - --file/-F
    - --template/-t
    - --cleanup
    - --untracked-file/-u
    - --gpg-sign/-S

--- git.zsh
+++ git.zsh
@@ -24,6 +24,8 @@ _fzf_complete_preview_git_diff='
 '
 
 _fzf_complete_git() {
+    local last_options=${${(z)LBUFFER}[-2]}
+
     if [[ "$@" =~ '^git (checkout|log|rebase|reset)' ]]; then
         _fzf_complete_git-commits '' "$@"
         return
@@ -35,13 +37,85 @@ _fzf_complete_git() {
     fi
 
     if [[ "$@" = 'git commit'* ]]; then
-        if [[ "$prefix" =~ '^--fixup=' ]]; then
+        if [[ "$prefix" =~ '^--(reuse-message|reedit-message|fixup|squash)=' ]]; then
             prefix_option="${prefix/=*/=}"
             _fzf_complete_git-commits '' "$@"
             unset prefix_option
-        else
-            _fzf_complete_git-commit-messages '' "$@"
+            return
         fi
+
+        if [[ "$last_options" =~ '^(-[^-]*[cC]|--(reuse-message|reedit-message|fixup|squash))$' ]]; then
+            _fzf_complete_git-commits '' "$@"
+            return
+        else
+        fi
+
+        if [[ "$prefix" =~ '^--message=' ]]; then
+            prefix_option="${prefix/=*/=}"
+            _fzf_complete_git-commit-messages '' "$@"
+            unset prefix_option
+            return
+        fi
+
+        if [[ "$last_options" =~ '^(-[^-]*m|--message)$' ]]; then
+            _fzf_complete_git-commit-messages '' "$@"
+            return
+        fi
+
+        if [[ "$prefix" =~ '^--author=' ]]; then
+            return
+        fi
+
+        if [[ "$prefix" =~ '^--date=' ]]; then
+            return
+        fi
+
+        if [[ "$prefix" =~ '^--(file|template)=$' ]]; then
+            _fzf_path_completion '' "$@$prefix"
+            return
+        fi
+
+        if [[ "$last_options" =~ '^(-[^-]*[Ft]|--(file|template))' ]]; then
+            _fzf_path_completion '' "$@"
+            return
+        fi
+
+        local cleanup_mode="strip whitespace verbatim scissors default"
+        if [[ "$prefix" =~ '^--cleanup=' ]]; then
+            _fzf_complete '' "$@" < <(echo $cleanup_mode | awk -v RS=' ' -v prefix="${prefix/=*/=}" '{ print prefix $0 }')
+            return
+        fi
+
+        if [[ "$last_options" = '--cleanup' ]]; then
+            _fzf_complete '' "$@" < <(echo $cleanup_mode | tr ' ' '\n')
+            return
+        fi
+
+        local untracked_file_mode="no normal all"
+        if [[ "$prefix" =~ '^--untracked-files=' ]]; then
+            _fzf_complete '' "$@" < <(echo $untracked_file_mode | awk -v RS=' ' -v prefix="${prefix/=*/=}" '{ print prefix $0 }')
+            return
+        fi
+
+        if [[ "$last_options" =~ '^(-[^-]*u|--untracked-files)' ]]; then
+            _fzf_complete '' "$@" < <(echo $untracked_file_mode | tr ' ' '\n')
+            return
+        fi
+
+        local gpg_command=$(git config --global gpg.program || (which gpg > /dev/null && echo gpg))
+        if [[ "$prefix" =~ '^--gpg-sign=' ]]; then
+            prefix_option="${prefix/=*/=}"
+            _fzf_complete_git-gpg-key '' "$@"
+            unset prefix_option
+            return
+        fi
+
+        if [[ "$last_options" =~ '^(-[^-]*S|--gpg-sign)' ]]; then
+            _fzf_complete_git-gpg-key '' "$@"
+            return
+        fi
+
+        _fzf_complete_git-unstaged-files '--multi' "$@"
         return
     fi
 
@@ -74,14 +148,26 @@ _fzf_complete_git-commits_post() {
 _fzf_complete_git-commit-messages() {
     local fzf_options="$1"
     shift
-    _fzf_complete "--ansi --tiebreak=index $fzf_options" "$@" < <(git log --format='%h  %s' 2> /dev/null)
+    _fzf_complete "--ansi --tiebreak=index $fzf_options" "$@" < <(
+        git log --format='%h %s' 2> /dev/null |
+        awk -v prefix="$prefix_option" '
+            {
+                match($0, / /)
+                print $1, prefix substr($0, RSTART + RLENGTH)
+            }
+        ' |
+        _fzf_complete_git_tabularize
+    )
 }
 
 _fzf_complete_git-commit-messages_post() {
-    awk '{
-        $1 = ""
-        sub(/^ /, "", $0)
-        print "'\''" $0 "'\''"
+    awk -v prefix="$prefix_option" '{
+        match($0, /  /)
+        str = substr($0, RSTART + RLENGTH)
+        match(str, prefix)
+        str = substr(str, RSTART + RLENGTH)
+        gsub("'\''", "'\''\\'\'''\''", str)
+        print prefix "'\''" str "'\''"
     }'
 }
 
@@ -107,6 +193,50 @@ _fzf_complete_git-unstaged-files_post() {
 
     local filename=$(awk '{ print substr($0, 4) }')
     echo "${(q)filename}"
+}
+
+_fzf_complete_git-gpg-key() {
+    local fzf_options="$1"
+    shift
+    local global_key=$(git config --global user.signingkey)
+    local local_key=$(git config --local user.signingkey)
+    _fzf_complete "--ansi $fzf_options" "$@" < <(
+        [[ -n "$gpg_command" ]] && LANG=C "$gpg_command" --list-secret-keys --keyid-format LONG 2> /dev/null |
+        awk -v global_key="$global_key" -v local_key=$local_key -v prefix="$prefix_option" '
+            /^sec/ {
+                for (i = 1; ;) {
+                    if ($0 == "") {
+                        break
+                    }
+                    if ($1 == "uid") {
+                        uid = $3 " " $4
+                    }
+                    if ($1 == "sec" || $1 == "ssb") {
+                        sub(/.*\//, "", $2)
+                        keys[i] = $2
+                        types[i++] = $4
+                    }
+                    getline
+                }
+                for (j = 1; j < i; ++j) {
+                    message = ""
+                    if (keys[j] == global_key) {
+                        message = message " " "[Git Global]"
+                    }
+                    if (keys[j] == local_key) {
+                        message = message " " "[Git Local]"
+                    }
+                    print prefix keys[j], types[j], uid message
+                }
+                delete keys
+                delete types
+            }
+        ' | _fzf_complete_git_tabularize
+    )
+}
+
+_fzf_complete_git-gpg-key_post() {
+    awk '{ print $1 }'
 }
 
 _fzf_complete_git_tabularize() {
