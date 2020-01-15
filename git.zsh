1cdc8f3d668a78036c67903202ed0350c39e16af
Hexin <hexium310@gmail.com>
2020-01-15 22:02:29 +0900

    Fix bug where untracked files are not shown.

--- git.zsh
+++ git.zsh
@@ -173,7 +173,7 @@ _fzf_complete_git-commit-messages_post() {
 _fzf_complete_git-unstaged-files() {
     local fzf_options="$1"
     shift
-    _fzf_complete "--ansi $fzf_options" "$@" < <(git status --porcelain=v1 -z 2> /dev/null | xargs -0 -n 1 | awk \
+    _fzf_complete "--ansi $fzf_options" "$@" < <(git status --untracked-files=all --porcelain=v1 -z 2> /dev/null | xargs -0 -n 1 | awk \
         -v green="$(tput setaf 2)" \
         -v red="$(tput setaf 1)" \
         -v reset="$(tput sgr0)" '
