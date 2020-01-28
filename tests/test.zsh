0cce3ea384e65ebd9da7b804798abd067b94ba88
Hexin <hexium310@gmail.com>
2020-01-29 03:18:15 +0900

    Make `revolver stop` call

--- tests/test.zsh
+++ tests/test.zsh
@@ -3,5 +3,10 @@
 ROOT=${0:h:h:A}
 PATH=$ROOT/bin/revolver:$ROOT/bin/zunit:$PATH
 
+__zunit_output_temp_dir=$(mktemp -d)
+
 cd $ROOT/bin/zunit && ./build.zsh
-cd $ROOT && zunit ${@:-tests/*.zunit}
+cd $ROOT && __zunit_output_temp_dir=$__zunit_output_temp_dir zunit --output-html ${@:-tests/*.zunit}
+
+rm -rf $__zunit_output_temp_dir
+unset __zunit_output_temp_dir
