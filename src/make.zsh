8e500753e03263bc6970bec8734199ccddd3713a
himanoa <matsunoappy@gmail.com>
2020-01-30 01:24:58 +0900

    Implemented _fzf_complete_make

---
+++ src/make.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+
+_fzf_complete_make() {
+    _fzf_complete "--ansi --tiebreak=index $fzf_options" $@ < <(grep -E '^[a-zA-Z_-]+:.*?$$' Makefile | sort | awk -F ':' '{ print $1  }')
+}
