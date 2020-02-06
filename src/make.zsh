981a7d6fcc56f71618e88cf2daf83acf2bbba779
himanoa <matsunoappy@gmail.com>
2020-02-06 23:37:03 +0900

    Change from sort to uniq

--- src/make.zsh
+++ src/make.zsh
@@ -2,5 +2,5 @@
 
 
 _fzf_complete_make() {
-    _fzf_complete "--ansi --tiebreak=index $fzf_options" $@ < <(grep -E '^[a-zA-Z_-]+:.*?$$' Makefile | sort | awk -F ':' '{ print $1  }')
+    _fzf_complete "--ansi --tiebreak=index $fzf_options" $@ < <(grep -E '^[a-zA-Z_-]+:.*?$$' Makefile | uniq | awk -F ':' '{ print $1 }')
 }
