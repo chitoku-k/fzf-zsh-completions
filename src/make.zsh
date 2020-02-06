99e38e356b2e3dddb1643e26a0a28795ce4a1a8f
himanoa <matsunoappy@gmail.com>
2020-02-07 00:41:57 +0900

    Add teardown section

--- src/make.zsh
+++ src/make.zsh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
-
 _fzf_complete_make() {
     _fzf_complete "--ansi --tiebreak=index $fzf_options" $@ < <(grep -E '^[a-zA-Z_-]+:.*?$$' Makefile | uniq | awk -F ':' '{ print $1 }')
 }
