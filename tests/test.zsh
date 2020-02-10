21eda85e533c6c861deb584965adfd2f48cbae16
Hexin <hexium310@gmail.com>
2020-02-11 00:58:15 +0900

    Add regex option to tests/test.zsh

--- tests/test.zsh
+++ tests/test.zsh
@@ -3,5 +3,40 @@
 ROOT=${0:h:h:A}
 PATH=$ROOT/bin/revolver:$ROOT/bin/zunit:$PATH
 
-cd $ROOT/bin/zunit && ./build.zsh
-cd $ROOT && zunit ${@:-tests/*.zunit}
+setup() {
+    local test_args=()
+    zparseopts -D -E r+:=regex -regex+:=regex
+
+    if [[ -n $regex ]]; then
+        for (( i = 2; i <= ${#regex}; i = i + 2 )); do
+            local argument=(${(s:@:)${regex[$i]}})
+            local test_file
+            local test_name
+            if [[ ${#argument} == 1 ]]; then
+                test_file=''
+                test_name=${argument[1]}
+            else
+                test_file=${argument[1]}
+                test_name=${argument[2]}
+            fi
+            local pattern='^ *@test  *([^ ]'$test_name')  *\{ *(.*)$'
+            IFS=$'\n' local lines=($(grep -H -E $pattern ${test_file:-tests/*.zunit}))
+            local line
+            for line in $lines; do
+                test_args+=(${test_file:-${line%%:*}}@${^${line#*\'}%\'*})
+            done
+        done
+    fi
+
+    args=($@ ${test_args})
+}
+
+run() {
+    local args=()
+    setup $@
+
+    cd $ROOT/bin/zunit && ./build.zsh
+    cd $ROOT && zunit ${args:-tests/*.zunit}
+}
+
+run $@
