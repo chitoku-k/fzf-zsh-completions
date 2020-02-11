85c32d59cc560da19bcec048e23bf4f9af3af42b
Chitoku <chitoku-k@users.noreply.github.com>
2020-02-11 18:54:40 +0900

    Migrate tput to zsh colors (#53)

--- src/systemctl.zsh
+++ src/systemctl.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+autoload -U colors
+colors
+
 _fzf_complete_preview_systemctl_status=$(cat <<'PREVIEW_OPTIONS'
     --preview-window=right:70%:wrap
     --preview='echo {} | awk '\''{ print $2 }'\'' | xargs systemctl status --'
@@ -9,9 +12,9 @@ PREVIEW_OPTIONS
 _fzf_complete_systemctl() {
     _fzf_complete "--ansi --tiebreak=index $_fzf_complete_preview_systemctl_status $FZF_DEFAULT_OPTS" $@ < \
         <(systemctl list-units --full --no-legend --no-pager "$prefix*" | sort | awk \
-            -v green=$(tput setaf 2) \
-            -v red=$(tput setaf 1) \
-            -v reset=$(tput sgr0) '
+            -v green=${fg[green]} \
+            -v red=${fg[red]} \
+            -v reset=$reset_color '
             {
                 unitname = $1
                 status = $3
