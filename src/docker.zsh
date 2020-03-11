e40c0ca1c6f379bb2b6afc3b1027b11264c0e084
Hexin <hexium310@gmail.com>
2020-03-12 03:38:18 +0900

    Generalize tabularize function

--- src/docker.zsh
+++ src/docker.zsh
@@ -25,41 +25,10 @@ _fzf_complete_docker-images() {
 
     _fzf_complete "--ansi --tiebreak=index --header-lines=1 $fzf_options" $@ < <(
         docker images --format 'table {{.ID}};{{.Repository}};{{.Tag}};{{if .CreatedSince}}{{.CreatedSince}}{{else}}N/A{{end}};{{.Size}}' 2> /dev/null \
-            | FS=';' _fzf_complete_docker_tabularize $fg[yellow] $reset_color{,,}
+            | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,,}
     )
 }
 
 _fzf_complete_docker-images_post() {
     awk '{ print $1 }'
-}
-
-_fzf_complete_docker_tabularize() {
-    awk \
-        -v FS=${FS:- } \
-        -v colors_args=${(pj: :)@} \
-        -v reset=$reset_color '
-        BEGIN {
-            split(colors_args, colors, " ")
-        }
-        {
-            str = $0
-            for (i = 1; i <= length(colors); ++i) {
-                field_max[i] = length($i) > field_max[i] ? length($i) : field_max[i]
-                fields[NR, i] = $i
-                pos = index(str, FS)
-                str = substr(str, pos + 1)
-            }
-            if (pos != 0) {
-                fields[NR, i] = str
-            }
-        }
-        END {
-            for (i = 1; i <= NR; ++i) {
-                for (j = 1; j <= length(colors); ++j) {
-                    printf "%s%-" field_max[j] "s%s  ", colors[j], fields[i, j], reset
-                }
-                print fields[i, j]
-            }
-        }
-    '
 }
---
+++ src/utils.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+autoload -U colors
+colors
+
+_fzf_complete_tabularize() {
+    awk \
+        -v FS=${FS:- } \
+        -v colors_args=${(pj: :)@} \
+        -v reset=$reset_color '
+        BEGIN {
+            split(colors_args, colors, " ")
+        }
+        {
+            str = $0
+            for (i = 1; i <= length(colors); ++i) {
+                field_max[i] = length($i) > field_max[i] ? length($i) : field_max[i]
+                fields[NR, i] = $i
+                pos = index(str, FS)
+                str = substr(str, pos + 1)
+            }
+            if (pos != 0) {
+                fields[NR, i] = str
+            }
+        }
+        END {
+            for (i = 1; i <= NR; ++i) {
+                for (j = 1; j <= length(colors); ++j) {
+                    printf "%s%-" field_max[j] "s%s  ", colors[j], fields[i, j], reset
+                }
+                print fields[i, j]
+            }
+        }
+    '
+}
