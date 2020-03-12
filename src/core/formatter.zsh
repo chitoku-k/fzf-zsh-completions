3eca4c82e8d3564a57e534425c3d705a7a1fd10c
Hexin <hexium310@gmail.com>
2020-03-12 22:45:54 +0900

    Generalize tabularize function (#61)
    * Generalize tabularize function
    
    * Fix tests
    
    * Fix behaviour when _fzf_complete_tabularize take no arguments
    
    * Fix tests/_support/git
    
    * Move src/* to src/{completers,core}/
    
    * Fix behaviour when _fzf_complete_tabularize take no arguments

--- src/core/formatter.zsh
+++ src/core/formatter.zsh
@@ -3,37 +3,12 @@
 autoload -U colors
 colors
 
-_fzf_complete_docker() {
-    local subcommand=${${(Q)${(z)@}}[2]}
-
-    if [[ $subcommand =~ (create|history|run) ]]; then
-        _fzf_complete_docker-images '' $@
+_fzf_complete_tabularize() {
+    if [[ $# = 0 ]]; then
+        cat
         return
     fi
 
-    if [[ $subcommand =~ (rmi|save) ]]; then
-        _fzf_complete_docker-images '--multi' $@
-        return
-    fi
-
-    _fzf_path_completion "$prefix" $@
-}
-
-_fzf_complete_docker-images() {
-    local fzf_options=$1
-    shift 1
-
-    _fzf_complete "--ansi --tiebreak=index --header-lines=1 $fzf_options" $@ < <(
-        docker images --format 'table {{.ID}};{{.Repository}};{{.Tag}};{{if .CreatedSince}}{{.CreatedSince}}{{else}}N/A{{end}};{{.Size}}' 2> /dev/null \
-            | FS=';' _fzf_complete_docker_tabularize $fg[yellow] $reset_color{,,}
-    )
-}
-
-_fzf_complete_docker-images_post() {
-    awk '{ print $1 }'
-}
-
-_fzf_complete_docker_tabularize() {
     awk \
         -v FS=${FS:- } \
         -v colors_args=${(pj: :)@} \
