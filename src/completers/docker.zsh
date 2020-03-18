e4945b8b8c1021200f3a16364146b1ab19168677
Hexin <hexium310@gmail.com>
2020-03-19 03:32:03 +0900

    Add completions using `docker container list`

--- src/completers/docker.zsh
+++ src/completers/docker.zsh
@@ -16,6 +16,26 @@ _fzf_complete_docker() {
         return
     fi
 
+    if [[ $subcommand =~ (attach|exec|top) ]]; then
+        _fzf_complete_docker-containers '' '' $@
+        return
+    fi
+
+    if [[ $subcommand =~ (kill|pause|stop|unpause) ]]; then
+        _fzf_complete_docker-containers '' '--multi' $@
+        return
+    fi
+
+    if [[ $subcommand =~ (commit|cp|diff|export|logs|port|rename) ]]; then
+        _fzf_complete_docker-containers '--all' '' $@
+        return
+    fi
+
+    if [[ $subcommand =~ (restart|rm|start|stats|update|wait) ]]; then
+        _fzf_complete_docker-containers '--all' '--multi' $@
+        return
+    fi
+
     _fzf_path_completion "$prefix" $@
 }
 
@@ -30,5 +50,21 @@ _fzf_complete_docker-images() {
 }
 
 _fzf_complete_docker-images_post() {
+    awk '{ print $1 }'
+}
+
+_fzf_complete_docker-containers() {
+    local docker_options=$1
+    local fzf_options=$2
+    shift 2
+
+    _fzf_complete "--ansi --tiebreak=index --header-lines=1 $fzf_options" $@ < <(
+        docker container list ${(Z+n+)docker_options} \
+            --format 'table {{.ID}};{{.Image}};{{.Command}};{{.RunningFor}};{{.Status}};{{.Ports}};{{.Names}}' 2> /dev/null \
+                | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,,,,}
+    )
+}
+
+_fzf_complete_docker-containers_post() {
     awk '{ print $1 }'
 }
