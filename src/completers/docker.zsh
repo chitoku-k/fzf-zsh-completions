d52c0b1c35cb317d0364db91181beda7413f3777
Hexin <hexium310@gmail.com>
2020-03-22 03:57:25 +0900

    Don't show dangling images

--- src/completers/docker.zsh
+++ src/completers/docker.zsh
@@ -72,7 +72,7 @@ _fzf_complete_docker-images-repository() {
     shift 1
 
     _fzf_complete "--ansi --tiebreak=index --header-lines=1 $fzf_options" $@ < <(
-        docker images --format 'table {{.Repository}};{{.ID}};{{.Tag}};{{if .CreatedSince}}{{.CreatedSince}}{{else}}N/A{{end}};{{.Size}}' 2> /dev/null \
+        docker images --filter 'dangling=false' --format 'table {{.Repository}};{{.ID}};{{.Tag}};{{if .CreatedSince}}{{.CreatedSince}}{{else}}N/A{{end}};{{.Size}}' 2> /dev/null \
             | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,,}
     )
 }
