f9749cdaf096d3dc0cbf45dc44e2e7f6aeae54fa
Chitoku <odango@chitoku.jp>
2020-04-26 23:35:07 +0900

    Implement docker inspect --type=network

--- src/completers/docker.zsh
+++ src/completers/docker.zsh
@@ -49,20 +49,25 @@ _fzf_complete_docker() {
     if [[ $subcommand = 'inspect' ]]; then
         local inspect_type_idx=${${(Q)${(z)arguments}}[(i)--type]}
 
-        if [[ -n ${${(Q)${(z)arguments}}[(r)--type=task]} ]] || [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'task' ]]; then
+        if [[ -z ${${(Q)${(z)arguments}}[(r)--type*]} ]] ||
+            [[ -n ${${(Q)${(z)arguments}}[(r)--type=container]} ]] ||
+            [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'container' ]]; then
+            _fzf_complete_docker-containers '--all' '--multi' $@
             return
         fi
 
-        if [[ -n ${${(Q)${(z)arguments}}[(r)--type=network]} ]] || [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'network' ]]; then
-            return
-        fi
-
-        if [[ -n ${${(Q)${(z)arguments}}[(r)--type=image]} ]] || [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'image' ]]; then
+        if [[ -n ${${(Q)${(z)arguments}}[(r)--type=image]} ]] ||
+            [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'image' ]]; then
             _fzf_complete_docker-images '--multi' $@
             return
         fi
 
-        _fzf_complete_docker-containers '--all' '--multi' $@
+        if [[ -n ${${(Q)${(z)arguments}}[(r)--type=network]} ]] ||
+            [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'network' ]]; then
+            _fzf_complete_docker-networks '--multi' $@
+            return
+        fi
+
         return
     fi
 
@@ -132,4 +137,24 @@ _fzf_complete_docker-containers_post() {
     else
         echo $input
     fi
+}
+
+_fzf_complete_docker-networks() {
+    local fzf_options=$1
+    shift
+
+    _fzf_complete --ansi --tiebreak=index --header-lines=1 ${(Q)${(Z+n+)fzf_options}} -- $@ < <(
+        docker network list --format 'table {{.ID}};{{.Name}};{{.Driver}};{{.Scope}}' 2> /dev/null \
+            | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,}
+    )
+}
+
+_fzf_complete_docker-networks_post() {
+    local input=$(awk '{ print $1 }')
+
+    if [[ -z $input ]]; then
+        return
+    fi
+
+    echo -n $input
 }
