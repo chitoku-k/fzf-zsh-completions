b6804bd64faa6912408dfaa4f80c3494a88e7a53
Chitoku <chitoku-k@users.noreply.github.com>
2020-04-27 15:12:30 +0900

    Support `docker inspect **` (#78)
    * Support `docker inspect **`
    
    * Implement docker inspect --type=network
    
    * Fix postprocessor for docker networks
    
    * Implement docker inspect --type=volume

--- src/completers/docker.zsh
+++ src/completers/docker.zsh
@@ -4,6 +4,7 @@ autoload -U colors
 colors
 
 _fzf_complete_docker() {
+    local arguments=$@
     local subcommand=${${(Q)${(z)@}}[2]}
 
     if [[ $subcommand =~ ^(create|history|run)$ ]]; then
@@ -11,7 +12,7 @@ _fzf_complete_docker() {
         return
     fi
 
-    if [[ $subcommand = push ]]; then
+    if [[ $subcommand = 'push' ]]; then
         _fzf_complete_docker-images-repository '' $@
         return
     fi
@@ -36,12 +37,43 @@ _fzf_complete_docker() {
         return
     fi
 
-    if [[ $subcommand = cp ]]; then
+    if [[ $subcommand = 'cp' ]]; then
         if [[ $prefix = */* ]]; then
             _fzf_path_completion "$prefix" $@
         else
             _fzf_complete_docker-containers '--all' '' $@
         fi
+        return
+    fi
+
+    if [[ $subcommand = 'inspect' ]]; then
+        local inspect_type_idx=${${(Q)${(z)arguments}}[(i)--type]}
+
+        if [[ -z ${${(Q)${(z)arguments}}[(r)--type*]} ]] ||
+            [[ -n ${${(Q)${(z)arguments}}[(r)--type=container]} ]] ||
+            [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'container' ]]; then
+            _fzf_complete_docker-containers '--all' '--multi' $@
+            return
+        fi
+
+        if [[ -n ${${(Q)${(z)arguments}}[(r)--type=image]} ]] ||
+            [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'image' ]]; then
+            _fzf_complete_docker-images '--multi' $@
+            return
+        fi
+
+        if [[ -n ${${(Q)${(z)arguments}}[(r)--type=network]} ]] ||
+            [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'network' ]]; then
+            _fzf_complete_docker-networks '--multi' $@
+            return
+        fi
+
+        if [[ -n ${${(Q)${(z)arguments}}[(r)--type=volume]} ]] ||
+            [[ ${${(Q)${(z)arguments}}[inspect_type_idx+1]} = 'volume' ]]; then
+            _fzf_complete_docker-volumes '--multi' $@
+            return
+        fi
+
         return
     fi
 
@@ -106,9 +138,37 @@ _fzf_complete_docker-containers_post() {
         return
     fi
 
-    if [[ $subcommand = cp ]]; then
+    if [[ $subcommand = 'cp' ]]; then
         echo -n $input:
     else
         echo $input
     fi
+}
+
+_fzf_complete_docker-networks() {
+    local fzf_options=$1
+    shift
+
+    _fzf_complete --ansi --tiebreak=index --header-lines=1 ${(Q)${(Z+n+)fzf_options}} -- $@ < <(
+        docker network list --format 'table {{.ID}};{{.Name}};{{.Driver}};{{.Scope}}' 2> /dev/null \
+            | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,}
+    )
+}
+
+_fzf_complete_docker-networks_post() {
+    awk '{ print $1 }'
+}
+
+_fzf_complete_docker-volumes() {
+    local fzf_options=$1
+    shift
+
+    _fzf_complete --ansi --tiebreak=index --header-lines=1 ${(Q)${(Z+n+)fzf_options}} -- $@ < <(
+        docker volume list --format 'table {{.Name}};{{.Driver}};{{.Scope}}' 2> /dev/null \
+            | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color
+    )
+}
+
+_fzf_complete_docker-volumes_post() {
+    awk '{ print $1 }'
 }
