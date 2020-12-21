32161611c2008152b693921716fa828a79ab53fe
himanoa <matsunoappy@gmail.com>
2020-12-22 03:16:20 +0900

    Support `yarn workspace **` (#99)
    * Support `yarn workspace **`
    
    * Enable null character delimiter
    
    * Update tests/_support/yarn/workspace/package.json
    
    Co-authored-by: Chitoku <chitoku-k@users.noreply.github.com>
    
    * Update tests/yarn.zunit
    
    Co-authored-by: Chitoku <chitoku-k@users.noreply.github.com>
    
    * Update src/completers/yarn.zsh
    
    Co-authored-by: Chitoku <chitoku-k@users.noreply.github.com>
    
    * Update src/completers/yarn.zsh
    
    Co-authored-by: Chitoku <chitoku-k@users.noreply.github.com>
    
    * Fix a test case
    
    * Update tests/_support/yarn/workspace/package.json
    
    * Add a new subcommand in README.md
    
    Co-authored-by: Chitoku <chitoku-k@users.noreply.github.com>

--- src/completers/yarn.zsh
+++ src/completers/yarn.zsh
@@ -1,10 +1,29 @@
 #!/usr/bin/env zsh
 
 _fzf_complete_yarn() {
+    if [[ $@ = 'yarn workspace ' ]]; then
+        _fzf_complete_yarn-workspace '' $@
+        return
+    fi
+
     if [[ $@ = 'yarn ' ]] || [[ $@ = 'yarn run ' ]]; then
         _fzf_complete_npm-run '' $@
         return
     fi
 
     _fzf_path_completion "$prefix" $@
+}
+
+_fzf_complete_yarn-workspace() {
+    local fzf_options=$1
+    shift
+
+    local parent_package=$(dirname -- $(npm root))/package.json
+    if [[ ! -f $parent_package ]]; then
+        return
+    fi
+
+    local workspace_packages_patterns=$(jq -r '.workspaces | map(. + "/package.json") | join("\u0000")' "$parent_package")
+
+   _fzf_complete --ansi --tiebreak=index ${(Q)${(Z+n+)fzf_options}} -- $@ < <(jq -r '.name' ${~${(0)workspace_packages_patterns}})
 }
