73a86a4fa9af538414f54657b2e31f11ea363824
Chitoku <odango@chitoku.jp>
2021-05-13 00:31:49 +0900

    Support rich assertion messages

--- tests/_helpers/assertions.zsh
+++ tests/_helpers/assertions.zsh
@@ -18,3 +18,28 @@ _zunit_assert_mock_times() {
         fi
     done
 }
+
+_zunit_post_assert() {
+    local -A replacements=(
+        \$reset_color $reset_color
+        \$bold_color $bold_color
+    )
+
+    local var key value name target
+    for var in fg fg_bold fg_no_bold bg bg_bold bg_no_bold; do
+        for key in ${(k)${(P)var}}; do
+            name="\$$var\[$key\]"
+            replacements[$name]=${${(P)var}[$key]}
+        done
+    done
+
+    local input=$(cat)
+    for key in ${(k)replacements}; do
+        target=${key//\\/}
+        [[ -z $tap ]] && target='\e[4;38;5;242m'$target'\e[4;31m'
+
+        input=${input//${replacements[$key]}/$target}
+    done
+
+    echo -n - $input
+}
