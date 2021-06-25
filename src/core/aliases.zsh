73e085917760d133c3301d8713253f5b675efd2b
Chitoku <odango@chitoku.jp>
2021-06-25 21:43:30 +0900

    Support subcommands in alias mapping

--- src/core/aliases.zsh
+++ src/core/aliases.zsh
@@ -1,14 +1,22 @@
 _fzf_complete_enable_aliases() {
-    local expr name value
+    local expr name value completer
     local completers=(${@:t:r})
 
     local IFS=$'\n'
     for expr in $(alias); do
         name=${expr%%=*}
         value=(${(Q)${(z)${(Q)${(z)expr##*=}}}})
+        completer=${completers[(r)$value[1]]}
+        arguments=${(@)value[2,-1]}
 
-        if [[ -n ${completers[(r)$value[1]]} ]]; then
-            eval "_fzf_complete_$name() { _fzf_complete_$value \$@ }"
+        if [[ -n $completer ]]; then
+            eval "
+                _fzf_complete_$name() {
+                    LBUFFER=\"\${LBUFFER/$name/$completer $arguments}\"
+                    _fzf_complete_$completer \${@/$name/$completer $arguments}
+                    LBUFFER=\"\${LBUFFER/$completer $arguments/$name}\"
+                }
+            "
         fi
     done
 }
