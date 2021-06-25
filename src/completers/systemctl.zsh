ac0f0b7a8d9696acc61a93ed76f81a6a23b54f10
Chitoku <chitoku-k@users.noreply.github.com>
2021-06-26 02:45:55 +0900

    Support --user and inactive units (#136)
    * Support --user and inactive units
    
    * Fix mock errors to be shown when mock is executed in subshell
    
    * Remove unnecessary call to awk

--- src/completers/systemctl.zsh
+++ src/completers/systemctl.zsh
@@ -5,29 +5,40 @@ colors
 
 _fzf_complete_preview_systemctl_status=$(cat <<'PREVIEW_OPTIONS'
     --preview-window=right:70%:wrap
-    --preview='echo {} | awk '\''{ print $2 }'\'' | xargs systemctl status --'
+    --preview='echo {} | awk '\''{ print $2 }'\'' | SYSTEMD_COLORS=true xargs systemctl status $SYSTEMCTL_OPTIONS --'
 PREVIEW_OPTIONS
 )
 
 _fzf_complete_systemctl() {
-    _fzf_complete --ansi --tiebreak=index ${(Q)${(Z+n+)_fzf_complete_preview_systemctl_status}} ${(Q)${(Z+n+)FZF_DEFAULT_OPTS}} -- $@ < \
-        <(systemctl list-units --full --no-legend --no-pager "$prefix*" | LC_ALL=C sort | awk \
-            -v green=${fg[green]} \
-            -v red=${fg[red]} \
-            -v reset=$reset_color '
-            {
-                unitname = $1
-                status = $3
+    local arguments=$@
+    local systemctl_options=(--full --no-legend --no-pager)
+    systemctl_options+=($(_fzf_complete_parse_option '' '--user --system' '' $arguments)) || :
 
-                if (status == "active") {
-                    active_color = green
-                }
-                if (status == "failed") {
-                    active_color = red
-                }
+    _fzf_complete --ansi --tiebreak=index ${(Q)${(Z+n+)${_fzf_complete_preview_systemctl_status/\$SYSTEMCTL_OPTIONS/$systemctl_options}}} ${(Q)${(Z+n+)FZF_DEFAULT_OPTS}} -- $@ < \
+        <({
+            systemctl list-units ${(Q)${(Z+n+)systemctl_options}} "$prefix*"
+            systemctl list-unit-files ${(Q)${(Z+n+)systemctl_options}} "$prefix*"
+        } |
+            LC_ALL=C sort -b -f -k 1,1 -k 3,3r |
+            awk \
+                -v green=${fg[green]} \
+                -v red=${fg[red]} \
+                -v reset=$reset_color '
+                $1 !~ /@\.(service|socket|target)$/ && !($1 in units) {
+                    unitname = $1
+                    status = $3
+                    units[unitname] = 1
 
-                printf("%s●%s %s\n", active_color, reset, unitname)
-            }')
+                    if (status == "active") {
+                        indicator = green "●" reset
+                    } else if (status == "failed") {
+                        indicator = red "●" reset
+                    } else {
+                        indicator = "○"
+                    }
+
+                    print indicator, unitname
+                }')
 }
 
 _fzf_complete_systemctl_post() {
