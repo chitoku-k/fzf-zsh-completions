fa605f88d320e1a94b451c46ab13908959f5e1aa
Chitoku <chitoku-k@users.noreply.github.com>
2021-06-25 23:04:28 +0900

    Map fzf completions for aliases (#134)
    * Map fzf completions for aliases
    
    * Support subcommands in alias mapping
    
    * Fix unintentionally exposed variable
    
    * Update documentation

---
+++ src/core/aliases.zsh
@@ -0,0 +1,24 @@
+_fzf_complete_enable_aliases() {
+    local expr name value completer arguments
+    local completers=(${@:t:r})
+
+    local IFS=$'\n'
+    for expr in $(alias); do
+        name=${expr%%=*}
+        value=(${(Q)${(z)${(Q)${(z)expr##*=}}}})
+        completer=${completers[(r)$value[1]]}
+        arguments=${(@)value[2,-1]}
+
+        if [[ -n $completer ]]; then
+            eval "
+                _fzf_complete_$name() {
+                    LBUFFER=\"\${LBUFFER/$name/$completer $arguments}\"
+                    _fzf_complete_$completer \${@/$name/$completer $arguments}
+                    LBUFFER=\"\${LBUFFER/$completer $arguments/$name}\"
+                }
+            "
+        fi
+    done
+}
+
+_fzf_complete_enable_aliases ${0:h:h}/completers/*
