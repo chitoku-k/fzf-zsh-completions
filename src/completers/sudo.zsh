0104b09094ab2fd383438e2040188fb2df673425
Chitoku <chitoku-k@users.noreply.github.com>
2021-06-25 23:39:51 +0900

    Add support for sudo **<TAB> (#135)
    * Add support for sudo **<TAB>
    
    * Add post assertion

---
+++ src/completers/sudo.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+_fzf_complete_sudo() {
+    local args=(${(Q)${(z)@}})
+    local subcommand=${args:1:1}
+
+    if (( $+functions[_fzf_complete_$subcommand] )); then
+        LBUFFER=${LBUFFER/sudo /}
+        _fzf_complete_$subcommand ${@/sudo /}
+        LBUFFER="sudo $LBUFFER"
+        return
+    fi
+
+    _fzf_path_completion "$prefix" $@
+}
