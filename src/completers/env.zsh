a90b6ee0ed632f36f400b85b77c5ffb04343a183
Hexin <hexium310@gmail.com>
2021-07-06 01:01:34 +0900

    Fix expansion

--- src/completers/env.zsh
+++ src/completers/env.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
 _fzf_complete_env() {
-    local arguments=(${(Q)${(z)@}})
+    local arguments=("${(Q)${(z)@}[@]}")
     arguments=($(_fzf_complete_trim_env "${arguments[2,-1]}"))
-    local cmd=${${${arguments}}[1]}
+    local cmd=${arguments[1]}
 
     if (( $+functions[_fzf_complete_$cmd] )); then
         LBUFFER=${LBUFFER/env /}
