8635ac9b5168527127a252bd9d854a7b072db086
Hexin <hexium310@gmail.com>
2021-07-07 01:55:01 +0900

    Disable aliases from expanding in function definitions

--- fzf-zsh-completions.plugin.zsh
+++ fzf-zsh-completions.plugin.zsh
@@ -1,4 +1,7 @@
-for f in ${0:h}/src/**/*.zsh(D); do
-    source "$f"
-done
-unset f
+() {
+    setopt local_options no_aliases
+    local f
+    for f in ${0:h}/src/**/*.zsh(D); do
+        source "$f"
+    done
+}
