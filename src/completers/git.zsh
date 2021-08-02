6b48f6e6f01eaadaab7c188679b62f886fc9292b
Hexin <hexium310@gmail.com>
2021-08-03 03:32:32 +0900

    Support git stash (#169)
    * Support git stash
    
    * Fix parsing short options
    
    * Fix condition
    
    * Add tests
    
    * List branches, tags and remotes only in git cherry-pick **
    
    * Move stash after show
    
    * Change color of second column

--- src/completers/git.zsh
+++ src/completers/git.zsh
@@ -693,6 +693,69 @@ _fzf_complete_git() {
         return
     fi
 
+    if [[ $subcommand = stash ]]; then
+        local prefix_option completing_option
+        local git_options_argument_required=(
+            --pathspec-from-file
+            -m
+            --message
+        )
+
+        if completing_option=$(_fzf_complete_parse_completing_option "$prefix" "$last_argument" "${(F)git_options_argument_required}" ''); then
+            if [[ $completing_option = --* ]]; then
+                prefix_option=$completing_option=
+            else
+                prefix_option=${prefix%%${completing_option[-1]}*}${completing_option[-1]}
+            fi
+            prefix=${prefix#$prefix_option}
+        fi
+
+        local stash_subcommand=${arguments[${arguments[(i)$subcommand]} + 1]}
+        case $stash_subcommand in
+            show)
+                _fzf_complete_git-stashes '' "$@"
+                return
+                ;;
+
+            apply|drop|pop)
+                _fzf_complete_git-stashes '' "$@"
+                return
+                ;;
+
+            branch)
+                if _fzf_complete_parse_argument 4 1 "${(F)git_options_argument_required}" "${arguments[@]}" > /dev/null; then
+                    _fzf_complete_git-stashes '' "$@"
+                    return
+                fi
+                ;;
+
+            *)
+                if [[ $stash_subcommand = push ]]; then
+                    case $completing_option in
+                        -m|--message)
+                            return
+                            ;;
+
+                        --pathspec-from-file)
+                            return
+                            ;;
+                    esac
+                fi
+
+                if [[ $stash_subcommand = push ]] || [[ ${arguments[(r)--]} = -- ]]; then
+                    local untracked_files
+                    if _fzf_complete_parse_option '-u' '--include-untracked' "${(F)git_options_argument_required}" "${arguments[@]}" > /dev/null; then
+                        untracked_files=all
+                    fi
+                    _fzf_complete_git-status-files 'unstaged' "--untracked-files=${untracked_files:-no}" "--multi $_fzf_complete_preview_git_diff $FZF_DEFAULT_OPTS" "$@"
+                    return
+                fi
+                ;;
+        esac
+
+        return
+    fi
+
     _fzf_path_completion "$prefix" "$@"
 }
 
@@ -726,7 +789,7 @@ _fzf_complete_git-commits-not-in-head() {
     local fzf_options=$1
     shift
 
-    local rev_list_all=$(git rev-list --all --oneline 2> /dev/null)
+    local rev_list_all=$(git rev-list --branches --tags --remotes --oneline 2> /dev/null)
     rev_list_all=(${(q)${(f)rev_list_all}})
     local rev_list_head=$(git rev-list HEAD --oneline 2> /dev/null)
     rev_list_head=(${(q)${(f)rev_list_head}})
@@ -956,6 +1019,19 @@ _fzf_complete_git-show-files_post() {
     for filename in ${(0)input}; do
         echo ${${(q+)filename}//\\n/\\\\n}
     done
+}
+
+_fzf_complete_git-stashes() {
+    local fzf_options=$1
+    shift
+
+    _fzf_complete --ansi ${(Q)${(Z+n+)fzf_options}} -- "$@$prefix_option" < <(
+        git stash list --format='%h %gd %gs' | _fzf_complete_tabularize ${fg[yellow]} ${fg[green]}
+    )
+}
+
+_fzf_complete_git-stashes_post() {
+    awk '{ print $1 }'
 }
 
 _fzf_complete_git_resolve_alias() {
