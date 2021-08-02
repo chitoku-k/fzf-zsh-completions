1056ef6ef7a777b50ee27071f3b9fe3089d85a23
Hexin <hexium310@gmail.com>
2021-08-03 03:02:53 +0900

    Move stash after show

--- src/completers/git.zsh
+++ src/completers/git.zsh
@@ -554,69 +554,6 @@ _fzf_complete_git() {
         return
     fi
 
-    if [[ $subcommand = stash ]]; then
-        local prefix_option completing_option
-        local git_options_argument_required=(
-            --pathspec-from-file
-            -m
-            --message
-        )
-
-        if completing_option=$(_fzf_complete_parse_completing_option "$prefix" "$last_argument" "${(F)git_options_argument_required}" ''); then
-            if [[ $completing_option = --* ]]; then
-                prefix_option=$completing_option=
-            else
-                prefix_option=${prefix%%${completing_option[-1]}*}${completing_option[-1]}
-            fi
-            prefix=${prefix#$prefix_option}
-        fi
-
-        local stash_subcommand=${arguments[${arguments[(i)$subcommand]} + 1]}
-        case $stash_subcommand in
-            show)
-                _fzf_complete_git-stashes '' "$@"
-                return
-                ;;
-
-            apply|drop|pop)
-                _fzf_complete_git-stashes '' "$@"
-                return
-                ;;
-
-            branch)
-                if _fzf_complete_parse_argument 4 1 "${(F)git_options_argument_required}" "${arguments[@]}" > /dev/null; then
-                    _fzf_complete_git-stashes '' "$@"
-                    return
-                fi
-                ;;
-
-            *)
-                if [[ $stash_subcommand = push ]]; then
-                    case $completing_option in
-                        -m|--message)
-                            return
-                            ;;
-
-                        --pathspec-from-file)
-                            return
-                            ;;
-                    esac
-                fi
-
-                if [[ $stash_subcommand = push ]] || [[ ${arguments[(r)--]} = -- ]]; then
-                    local untracked_files
-                    if _fzf_complete_parse_option '-u' '--include-untracked' "${(F)git_options_argument_required}" "${arguments[@]}" > /dev/null; then
-                        untracked_files=all
-                    fi
-                    _fzf_complete_git-status-files 'unstaged' "--untracked-files=${untracked_files:-no}" "--multi $_fzf_complete_preview_git_diff $FZF_DEFAULT_OPTS" "$@"
-                    return
-                fi
-                ;;
-        esac
-
-        return
-    fi
-
     if [[ $subcommand = show ]]; then
         local prefix_option completing_option
         local git_options_argument_required=(
@@ -750,6 +687,69 @@ _fzf_complete_git() {
 
                 _fzf_complete_git-commits '--multi' "$@"
                 return
+                ;;
+        esac
+
+        return
+    fi
+
+    if [[ $subcommand = stash ]]; then
+        local prefix_option completing_option
+        local git_options_argument_required=(
+            --pathspec-from-file
+            -m
+            --message
+        )
+
+        if completing_option=$(_fzf_complete_parse_completing_option "$prefix" "$last_argument" "${(F)git_options_argument_required}" ''); then
+            if [[ $completing_option = --* ]]; then
+                prefix_option=$completing_option=
+            else
+                prefix_option=${prefix%%${completing_option[-1]}*}${completing_option[-1]}
+            fi
+            prefix=${prefix#$prefix_option}
+        fi
+
+        local stash_subcommand=${arguments[${arguments[(i)$subcommand]} + 1]}
+        case $stash_subcommand in
+            show)
+                _fzf_complete_git-stashes '' "$@"
+                return
+                ;;
+
+            apply|drop|pop)
+                _fzf_complete_git-stashes '' "$@"
+                return
+                ;;
+
+            branch)
+                if _fzf_complete_parse_argument 4 1 "${(F)git_options_argument_required}" "${arguments[@]}" > /dev/null; then
+                    _fzf_complete_git-stashes '' "$@"
+                    return
+                fi
+                ;;
+
+            *)
+                if [[ $stash_subcommand = push ]]; then
+                    case $completing_option in
+                        -m|--message)
+                            return
+                            ;;
+
+                        --pathspec-from-file)
+                            return
+                            ;;
+                    esac
+                fi
+
+                if [[ $stash_subcommand = push ]] || [[ ${arguments[(r)--]} = -- ]]; then
+                    local untracked_files
+                    if _fzf_complete_parse_option '-u' '--include-untracked' "${(F)git_options_argument_required}" "${arguments[@]}" > /dev/null; then
+                        untracked_files=all
+                    fi
+                    _fzf_complete_git-status-files 'unstaged' "--untracked-files=${untracked_files:-no}" "--multi $_fzf_complete_preview_git_diff $FZF_DEFAULT_OPTS" "$@"
+                    return
+                fi
                 ;;
         esac
 
