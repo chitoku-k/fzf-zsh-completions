0158c297138aaf73d23f09cafae9543166b7d9a8
Hexin <hexium310@gmail.com>
2021-09-17 00:14:18 +0900

    Remove worse arithmetic evaluation

--- src/completers/env.zsh
+++ src/completers/env.zsh
@@ -4,7 +4,7 @@ _fzf_complete_env() {
     setopt local_options no_aliases
     local arguments=("${(Q)${(z)@}[@]}")
     local command_pos=$(_fzf_complete_get_command_pos "${arguments[2,-1]}")
-    arguments=("${(Q)${(z)"$(_fzf_complete_trim_env "$(( $command_pos + 1 ))" "${arguments[2,-1]}")"}[@]}")
+    arguments=("${(Q)${(z)"$(_fzf_complete_trim_env "$command_pos" "${arguments[2,-1]}")"}[@]}")
     local cmd=${arguments[1]}
 
     if (( $+functions[_fzf_complete_$cmd] )); then
