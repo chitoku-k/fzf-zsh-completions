6ab7525a03ed128d0861b57ebdaaadd2da937d93
Chitoku <odango@chitoku.jp>
2023-07-11 01:02:33 +0900

    Run unit tests on Ubuntu 16.04 LTS

---
+++ tests/versions.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+group() {
+    local name=$1
+    shift
+
+    [[ $GITHUB_ACTIONS = true ]] && echo ::group::$name || echo "=== $name ==="
+
+    cat -- "$@"
+
+    [[ $GITHUB_ACTIONS = true ]] && echo ::endgroup:: || echo
+}
+
+group zsh <(zsh --version)
+group jq <(jq --version)
+group awk <(awk --version 2> /dev/null || awk -W version)
+group git <(git --version)
+group node <(node --version)
+group yarn <(yarn --version)
+group php <(php --version)
