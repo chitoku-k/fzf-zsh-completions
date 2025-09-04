738a169acf21b36c6be431f905743cab8987bcc0
Hexin <hexium310@gmail.com>
2025-09-05 08:00:09 +0900

    Apply --context and --host to completion context for docker

--- src/completers/docker.zsh
+++ src/completers/docker.zsh
@@ -7,47 +7,82 @@ _fzf_complete_docker() {
     setopt local_options no_aliases
     local command_pos=$(_fzf_complete_get_command_pos "$@")
     local arguments=("${(Qe)${(z)$(_fzf_complete_requote_arguments ${~${(z)@}})}[@][$command_pos, -1]}")
-    local subcommand=${arguments[2]}
 
     if (( $command_pos > 1 )); then
         local -x "${(Qe)${(z)$(_fzf_complete_requote_arguments ${~${(z)@}})}[@][1, $command_pos - 1]}"
     fi
 
-    if (( $+functions[_fzf_complete_docker_${subcommand}] )) && _fzf_complete_docker_${subcommand} "$@"; then
-        return
+    local options_and_subcommand subcommands
+    local docker_default_global_arguments=()
+    local docker_options_argument_optional=()
+    local docker_options_argument_required=(
+        -c
+        --context
+        -H
+        --host
+    )
+
+    if options_and_subcommand=("${(Q)${(z)"$(_fzf_complete_parse_global_options_and_subcommand "${(F)docker_options_argument_optional}" "${arguments[@]}")"}[@]}"); then
+        subcommands=("${options_and_subcommand[-1]}")
+        arguments=(
+            "${arguments[1,1][@]}"
+            "${subcommands[1,1][@]}"
+            "${options_and_subcommand[1,-2][@]}"
+            "${arguments[${#options_and_subcommand}+2,-1][@]}"
+        )
+
+        local context
+        if context=$(_fzf_complete_parse_option_arguments '-c' '--context' "${(F)docker_options_argument_required}" 'argument' "${arguments[@]}"); then
+            docker_default_global_arguments+=(
+                --context
+                "$context"
+            )
+        fi
+
+        local host
+        if host=$(_fzf_complete_parse_option_arguments '-H' '--host' "${(F)docker_options_argument_required}" 'argument' "${arguments[@]}"); then
+            docker_default_global_arguments+=(
+                --host
+                "$host"
+            )
+        fi
+
+        if (( $+functions[_fzf_complete_docker_${subcommands[1]}] )) && _fzf_complete_docker_${subcommands[1]} "$@"; then
+            return
+        fi
     fi
 
-    if [[ $subcommand = (create|history|run) ]]; then
+    if [[ ${subcommands[1]} = (create|history|run) ]]; then
         _fzf_complete_docker-images '' "$@"
         return
     fi
 
-    if [[ $subcommand = push ]]; then
+    if [[ ${subcommands[1]} = push ]]; then
         _fzf_complete_docker-images-repository '' "$@"
         return
     fi
 
-    if [[ $subcommand = (rmi|save|tag) ]]; then
+    if [[ ${subcommands[1]} = (rmi|save|tag) ]]; then
         _fzf_complete_docker-images '--multi' "$@"
         return
     fi
 
-    if [[ $subcommand = (attach|exec|top) ]]; then
+    if [[ ${subcommands[1]} = (attach|exec|top) ]]; then
         _fzf_complete_docker-containers '' '' "$@"
         return
     fi
 
-    if [[ $subcommand = (kill|pause|stop|unpause) ]]; then
+    if [[ ${subcommands[1]} = (kill|pause|stop|unpause) ]]; then
         _fzf_complete_docker-containers '' '--multi' "$@"
         return
     fi
 
-    if [[ $subcommand = (commit|diff|export|logs|port|rename) ]]; then
+    if [[ ${subcommands[1]} = (commit|diff|export|logs|port|rename) ]]; then
         _fzf_complete_docker-containers '--all' '' "$@"
         return
     fi
 
-    if [[ $subcommand = cp ]]; then
+    if [[ ${subcommands[1]} = cp ]]; then
         if [[ $prefix = */* ]]; then
             _fzf_path_completion "$prefix" "$@"
         else
@@ -56,7 +91,7 @@ _fzf_complete_docker() {
         return
     fi
 
-    if [[ $subcommand = inspect ]]; then
+    if [[ ${subcommands[1]} = inspect ]]; then
         local inspect_type=$(_fzf_complete_parse_option_arguments '' '--type' '--type' 'argument' "${arguments[@]}" || :)
 
         case $inspect_type in
@@ -87,7 +122,7 @@ _fzf_complete_docker() {
         return
     fi
 
-    if [[ $subcommand = (restart|rm|start|stats|update|wait) ]]; then
+    if [[ ${subcommands[1]} = (restart|rm|start|stats|update|wait) ]]; then
         _fzf_complete_docker-containers '--all' '--multi' "$@"
         return
     fi
@@ -100,7 +135,7 @@ _fzf_complete_docker-images() {
     shift 1
 
     _fzf_complete --ansi --tiebreak=index --header-lines=1 ${(Q)${(Z+n+)fzf_options}} -- "$@" < <(
-        docker images --format 'table {{.ID}};{{.Repository}};{{.Tag}};{{if .CreatedSince}}{{.CreatedSince}}{{else}}N/A{{end}};{{.Size}}' 2> /dev/null \
+        docker ${(Q)${(z)docker_default_global_arguments}} images --format 'table {{.ID}};{{.Repository}};{{.Tag}};{{if .CreatedSince}}{{.CreatedSince}}{{else}}N/A{{end}};{{.Size}}' 2> /dev/null \
             | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,,}
     )
 }
@@ -114,7 +149,7 @@ _fzf_complete_docker-images-repository() {
     shift 1
 
     _fzf_complete --ansi --tiebreak=index --header-lines=1 ${(Q)${(Z+n+)fzf_options}} -- "$@" < <(
-        docker images --filter 'dangling=false' --format 'table {{.Repository}};{{.ID}};{{.Tag}};{{if .CreatedSince}}{{.CreatedSince}}{{else}}N/A{{end}};{{.Size}}' 2> /dev/null \
+        docker ${(Q)${(z)docker_default_global_arguments}} images --filter 'dangling=false' --format 'table {{.Repository}};{{.ID}};{{.Tag}};{{if .CreatedSince}}{{.CreatedSince}}{{else}}N/A{{end}};{{.Size}}' 2> /dev/null \
             | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,,}
     )
 }
@@ -135,7 +170,7 @@ _fzf_complete_docker-containers() {
     shift 2
 
     _fzf_complete --ansi --tiebreak=index --header-lines=1 ${(Q)${(Z+n+)fzf_options}} -- "$@" < <(
-        docker container list ${(Q)${(Z+n+)docker_options}} \
+        docker ${(Q)${(z)docker_default_global_arguments}} container list ${(Q)${(Z+n+)docker_options}} \
             --format 'table {{.ID}};{{.Image}};{{.Command}};{{.RunningFor}};{{.Status}};{{.Ports}};{{.Names}}' 2> /dev/null \
                 | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,,,,}
     )
@@ -148,7 +183,7 @@ _fzf_complete_docker-containers_post() {
         return
     fi
 
-    if [[ $subcommand = cp ]]; then
+    if [[ ${subcommands[1]} = cp ]]; then
         echo -n $input:
     else
         echo $input
@@ -160,7 +195,7 @@ _fzf_complete_docker-networks() {
     shift
 
     _fzf_complete --ansi --tiebreak=index --header-lines=1 ${(Q)${(Z+n+)fzf_options}} -- "$@" < <(
-        docker network list --format 'table {{.ID}};{{.Name}};{{.Driver}};{{.Scope}}' 2> /dev/null \
+        docker ${(Q)${(z)docker_default_global_arguments}} network list --format 'table {{.ID}};{{.Name}};{{.Driver}};{{.Scope}}' 2> /dev/null \
             | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color{,}
     )
 }
@@ -174,7 +209,7 @@ _fzf_complete_docker-volumes() {
     shift
 
     _fzf_complete --ansi --tiebreak=index --header-lines=1 ${(Q)${(Z+n+)fzf_options}} -- "$@" < <(
-        docker volume list --format 'table {{.Name}};{{.Driver}};{{.Scope}}' 2> /dev/null \
+        docker ${(Q)${(z)docker_default_global_arguments}} volume list --format 'table {{.Name}};{{.Driver}};{{.Scope}}' 2> /dev/null \
             | FS=';' _fzf_complete_tabularize $fg[yellow] $reset_color
     )
 }
